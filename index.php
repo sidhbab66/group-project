9857c1d8a4fd8ff35fb403998a3ab273d812d433
Sid <s.h.hosseyni.a@gmail.com>
2023-01-26 14:19:04 +0330

    initial commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <a href="mjavad.html">MohamadJavad</a>
+  <a href="hasan.html">Sid</a>
+  <a href="alireza.html">AliReza</a>
+  <a href="ali.html">Ali</a>
+</body>
+</html>
